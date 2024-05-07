011a2985303e73502d4fb1c55ca81e70023e531d
Alexandr Yumanov <alexandryumanov@Alexandrs-MacBook-Air.local>
2024-05-07 22:04:36 +0300

    2commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,16 +2,4 @@
 const nextConfig = {};
 
 export default nextConfig;
-
-// module.exports = {
-//     images: {
-//       remotePatterns: [
-//         {
-//           protocol: 'https',
-//           hostname: 'media.themoviedb.org',
-//           port: '',
-//           pathname: 't/p/w300_and_h450_multi_faces_filter(none)/**',
-//         },
-//       ],
-//     },
-//   }
+outputFileTracing: false
