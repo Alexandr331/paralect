818c59a9aaf8bacd1f9632c9d71e28d491cf1fe7
Alexandr Yumanov <alexandryumanov@Alexandrs-MacBook-Air.local>
2024-05-07 23:04:28 +0300

    new bug fi

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -1,4 +1,4 @@
-import { useDispatch, useSelector, useStore } from 'react-redux'
+import { useDispatch, useSelector, useStore } from '../node_modules/react-redux'
 import type { AppDispatch, AppStore, RootState } from './store'
 
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
