a4b493c18df3b50b491ca76fa42aca40e10e9ec4
Alexandr Yumanov <alexandryumanov@Alexandrs-MacBook-Air.local>
2024-05-08 12:57:00 +0300

    redux fix#4

--- lib/features/movies/moviesSlice.ts
+++ lib/features/movies/moviesSlice.ts
@@ -1,6 +1,15 @@
 import { MovieDetail } from '@/app/page'
 import { Sort, getMoviesList, searchByTitle, urlString } from '@/app/actions'
 
+
+const options = {
+  method: 'GET',
+  headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`
+  }
+};
+
 export function searchMovies(queryStr: Sort, page?: number) {  
   return async function searchMoviesThunk(dispatch: any) {
     const response = await urlString(queryStr, page ?? 1)
@@ -10,9 +19,20 @@ export function searchMovies(queryStr: Sort, page?: number) {
 
 export function fetchMovies() {
   return async function fetchMoviesThunk(dispatch: any) {
-    const response = await getMoviesList()
-    dispatch({ type: 'fetchMovies', payload: response })
+    // const response = await getMoviesList()
+    try {
+      const response=  await fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+      if (response.ok) {
+        const data = await response.json()
+        dispatch({ type: 'fetchMovies', payload: data })
+      }
+      else throw new Error(response.statusText)   
+    } catch (error) {
+      console.error(error);
+    }
+    
   }
+  
 }
 
 let initialState: {results: MovieDetail[], total_pages: number, page: number} = {results: [], total_pages: 0, page: 0}
