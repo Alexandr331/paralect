7be7eaf9a7bd56d79ced294758cfbc1cb832b873
Alexandr Yumanov <alexandryumanov@Alexandrs-MacBook-Air.local>
2024-05-09 13:57:06 +0300

    fix #3

--- app/actions.tsx
+++ app/actions.tsx
@@ -12,7 +12,7 @@ const options = {
   method: 'GET',
   headers: {
       accept: 'application/json',
-      Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`
+      Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1N2NjZmZhYzkxNWM1Yjc0ODMwYWY1MzJiZjA1NTE4MyIsInN1YiI6IjY2MmE2M2FjNTBmN2NhMDBiNWM4OTJmNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ie239o2-bMwl9IKovKs8R6d7Jk2LuynONJKqqEEDl1A`
   }
 };
 
@@ -27,7 +27,6 @@ export async function searchMovie(queryStr: Sort, page: number) {
                       + (queryStr["vote_average.gte"] ? `&vote_average.gte=${queryStr["vote_average.gte"]}` : '') 
                       + (queryStr["vote_average.lte"] ? `&vote_average.lte=${queryStr["vote_average.lte"]}` : '') 
                       + (page ? `&page=${page}` : '&page=1')
-                      console.log(queryString);
     const response = await searchWithParams(queryString)
     return response
   }
@@ -48,7 +47,6 @@ export async function searchByTitle(title: string, page: number | 1) {
 export async function getMoviesList() {
   try {
     const response=  await fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
-    
     if (response.ok) {
       const data = await response.json()
       return data
